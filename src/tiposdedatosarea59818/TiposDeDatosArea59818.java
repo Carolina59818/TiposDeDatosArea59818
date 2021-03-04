136c602a520967963cb247e3cb8bfd0839c7ea42
caroc <caroc@192.168.1.71>
2021-03-03 19:54:54 -0600

    Lados y area de un rectangulo

---
+++ src/tiposdedatosarea59818/TiposDeDatosArea59818.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea59818;
+
+/**
+ *
+ * @author caroc
+ */
+public class TiposDeDatosArea59818 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int ladoA, ladoB; 
+        ladoA = 5; 
+        ladoB = 8;    
+        int area;  
+        area = (ladoA * ladoB);
+        System.out.println("El area del rectangulo es: " + area);
+    }
+    
+}
